9fde8915d79ec0b65fd33c5fcc6476d3e65f14cc
KangMin-gu <mincu1028@gmail.com>
2019-01-07 11:02:43 +0900

    mariadb 마이그레이션
    작업일시:2019.1.7
    작업자:강민구
    작업내용: mssql -> mariadb 마이그레이션을 위한 준비.
    2기종 설정은 아님.

--- CrudCrm/src/main/java/saas/crud/crm/util/Encryptor.java
+++ CrudCrm/src/main/java/saas/crud/crm/util/Encryptor.java
@@ -11,12 +11,12 @@ public class Encryptor {
 			        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
 			        pbeEnc.setPassword("jasyptPass"); // PBE 값(XML PASSWORD설정)
 			        
-			        String url = pbeEnc.encrypt("jdbc:sqlserver://125.129.242.156:1433;DatabaseName=Crud_SaaS");
-			        String username = pbeEnc.encrypt("sa");
+			        String url = pbeEnc.encrypt("jdbc:sqlserver://211.233.81.186:3306/Crud_SaaS");
+			        String username = pbeEnc.encrypt("root");
 			        String password = pbeEnc.encrypt("crudsystem1008!");
 			        
 			        String url2 = pbeEnc.decrypt("a8IzRdzVpQ+iPwrR4N4jpBEORCa0UyP9ENsh1fnNgAYLZs4gT3V5ZDpA3eflSKgXo1RSPG4R/YEOix10xDBCE32rmsoU5UoR");
-			        System.out.println("URL= "+url2);
+			        System.out.println("URL= "+url);
 			        System.out.println("USERNAME= "+username);
 			        System.out.println("PASSWORD= "+password);
     }

38492a053b1c7df9ef29d7f30f567cd22f8102f9
KangMin-gu <mincu1028@gmail.com>
2019-01-08 09:56:35 +0900

    mariadb 마이그레이션

--- CrudCrm/src/main/java/saas/crud/crm/util/Encryptor.java
+++ CrudCrm/src/main/java/saas/crud/crm/util/Encryptor.java
@@ -11,7 +11,7 @@ public class Encryptor {
 			        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
 			        pbeEnc.setPassword("jasyptPass"); // PBE 값(XML PASSWORD설정)
 			        
-			        String url = pbeEnc.encrypt("jdbc:sqlserver://211.233.81.186:3306/Crud_SaaS");
+			        String url = pbeEnc.encrypt("jdbc:sqlserver://125.129.242.156:3306/Crud_SaaS");
 			        String username = pbeEnc.encrypt("root");
 			        String password = pbeEnc.encrypt("crudsystem1008!");
 			        

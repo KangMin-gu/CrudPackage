3cc7ff06d47151587cc4bc4e71e118105ff6a1f0
sdwsgkj <sdwsgkj@naver.com>
2019-01-23 13:54:54 +0900

    임시커밋
    커밋

--- CrudCrm/src/main/java/saas/crud/crm/au/service/GoodsServiceImpl.java
+++ CrudCrm/src/main/java/saas/crud/crm/au/service/GoodsServiceImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import saas.crud.crm.au.dao.GoodsDao;
 import saas.crud.crm.au.dto.GoodsDto;
 
+@Service
 public class GoodsServiceImpl implements GoodsService{
 
 	@Autowired
